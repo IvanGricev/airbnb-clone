5b002a483a7d389e15334cc5df67a2f79b9d8cd8
Daxa <149832298+DaxaAAA@users.noreply.github.com>
2025-05-06 01:21:09 +0300

    Сделала футер

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,3 @@
-
 <footer class="footer">
     <div class="container">
         <div class="footer-content">
@@ -6,31 +5,27 @@
                 <div class="footer-logo">
                     <img src="{{ asset('images/logo.svg') }}" alt="Housing Hub">
                 </div>
-                <p class="footer-desc">
+                <p class="footer-desc" >
                     Мы помогаем найти дом вашей мечты или сдать недвижимость в аренду быстро, надежно и просто.
                 </p>
             </div>
             
-            <div class="footer-col">
-                <h4>Аренда</h4>
-                <ul>
+            <div class="footer-links-wrapper">
+                <div class="footer-links">
+                    <ul>
                     <li><a href="#">Поиск жилья</a></li>
                     <li><a href="#">Популярные районы</a></li>
                     <li><a href="#">Новые объявления</a></li>
                     <li><a href="#">Советы арендаторам</a></li>
-                </ul>
-            </div>
-
-            <div class="footer-col">
-                <h4>Предложения</h4>
-                <ul>
+                    </ul>
+                    <ul>
                     <li><a href="#">Разместить объявление</a></li>
                     <li><a href="#">Правила публикации</a></li>
                     <li><a href="#">Помощь арендодателям</a></li>
                     <li><a href="#">Тарифы и услуги</a></li>
-                </ul>
+                    </ul>
+                </div>
             </div>
-
         </div>
     </div>
 </footer>
@@ -45,8 +40,8 @@
 
 .footer-content {
     display: grid;
-    grid-template-columns: 1.5fr 1fr 1fr 1fr;
-    gap: 40px;
+    grid-template-columns: 1.5fr 1fr;
+    gap: 30px;
     margin-bottom: 40px;
 }
 
@@ -59,13 +54,8 @@
     color: rgba(90, 90, 90, 0.7);
     font-size: 0.95rem;
     line-height: 1.6;
-}
-
-.footer-col h4 {
-    color: #fff;
-    font-size: 1.1rem;
-    font-weight: 700;
-    margin-bottom: 20px;
+    max-width: 300px;
+    text-align: left;
 }
 
 .footer-col ul {
@@ -77,7 +67,40 @@
 .footer-col ul li {
     margin-bottom: 12px;
 }
-
 .footer-col ul li a {
     color: rgba(0, 0, 0, 0.7);
+}
 
+.footer-links-wrapper {
+    width: 100%;
+    display: flex;
+    justify-content: flex-stat;
+    margin-top: 40px;
+}
+
+.footer-links {
+    display: flex;
+    gap: 80px;
+}
+
+.footer-links ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+
+.footer-links li {
+    margin-bottom: 12px;
+}
+
+.footer-links a {
+    color: #232323;
+    text-decoration: none;
+    font-size: 1.0rem;
+    transition: color 0.2s;
+}
+
+.footer-links a:hover {
+    color: #ffb43a;
+}
+</style>
