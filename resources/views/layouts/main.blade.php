b559c775275dc6308f59f05a7b471679baabb93f
Ivan <145771734+IvanGricev@users.noreply.github.com>
2025-05-28 16:30:23 +0300

    styles fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,8 +14,8 @@
                 https://cdnjs.cloudflare.com;
     ">
 
-    <link rel="stylesheet" href="{{ url('/css/app.css') }}">
-    <link rel="stylesheet" href="{{ url('/css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     
     <!-- CDN FullCalendar CSS -->
@@ -36,7 +36,7 @@
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script type="module" src="{{ url('js/app.js') }}"></script>
+    <script type="module" src="{{ asset('js/app.js') }}"></script>
 
 </body>
 </html>
